1e069866d46424ab775a67714187524ac8d4860b
김명성 <ms@gimmyeongseong-ui-MacBookPro.local>
2021-08-03 10:30:49 +0900

    ADD : Main page 검색창 레이아웃 - input을 이용해 검색창 제작 - button을 input과 중첩해 제작 - 메인 이미지는 임의 이미지 button

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './Main.scss';
 
 class Main extends React.Component {
   render() {
     return (
       <div className="Main">
-        <div>Main 입니다</div>
+        <header className="MainHeader">
+          <div className="MainTopMent">
+            <h2>솔직한 리뷰, 믿을 수 있는 평점!</h2>
+            <h2>탱고플레이트</h2>
+            <form className="searchForm">
+              <FontAwesomeIcon icon={faSearch} className="searchIcon" />
+              <input
+                type="text"
+                className="searchInput"
+                maxLength="30"
+                placeholder="맛집"
+              ></input>
+              <button className="searchButton">검색</button>
+            </form>
+          </div>
+        </header>
       </div>
     );
   }
