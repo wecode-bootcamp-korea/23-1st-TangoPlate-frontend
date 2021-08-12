6c713b18afa592db4e73b44779f10fb40683c039
jungmin <minmin93240@google.com>
2021-08-12 19:23:56 +0900

    MODIFY : 가고싶다 모달창 수정

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
-export const SIGNIN_URL = 'http://10.58.0.54:8000/users/signin';
-export const SIGNUP_URL = 'http://10.58.0.54:8000/users/signup';
-export const NEWREVIEW_URL = 'http://10.58.0.54:8000/reviews/2';
-export const REVIEW_URL = 'http://10.58.0.54:8000/restaurants/2';
+const BASE_URL = 'http://10.58.0.96:8000';
+
+export const SIGNIN_URL = `${BASE_URL}/users/signin`;
+export const SIGNUP_URL = `${BASE_URL}/users/signup`;
+export const NEWREVIEW_URL = `${BASE_URL}/restaurant/`;
+export const RESTAURANT_DETAIL_URL = `${BASE_URL}/restaurant/1`;
+export const WISH_URL = `${BASE_URL}/users/restaurant/`;
+export const WISHED_LIST = `${BASE_URL}/users/wishlist`;
+// export const REVIEW_URL = 'http://10.58.2.156:8000/2';
--- src/pages/ShopDetail/ShopDetail.js
+++ src/pages/ShopDetail/ShopDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { REVIEW_URL } from '../../config';
+import { RESTAURANT_DETAIL_URL, WISH_URL } from '../../config';
 import RestaurantReview from './RestaurantReview/RestaurantReview';
 import RestaurantInfo from './RestaurantInfo/RestaurantInfo';
 import './ShopDetail.scss';
@@ -8,73 +8,91 @@ class ShopDetail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wanted: false,
       data: [],
       updateReviewdata: [],
       quantity: 9,
-      moreButtonHidden: false,
+      isMoreButtonHidden: false,
+      wanted: false,
     };
   }
   goToWrtingPage = () => {
-    this.props.history.push('/shopdetail-reviewwritingpage');
+    if (!localStorage.getItem('token')) {
+      alert('로그인이 필요합니다 !');
+    } else {
+      const edit = false;
+      this.props.history.push(
+        '/shopdetail-reviewwritingpage',
+        this.state.data.id,
+        edit
+      );
+    }
   };
 
-  //   handleWanted = () => {
-  //     let update = [...this.state.data];
-  //     update.
-  // this.setState({
-
-  // });
-  //    is_wished
-  //   };
+  handleWanted = () => {
+    if (!this.state.wanted) {
+      fetch(`${WISH_URL}${this.state.data.id}/wish`, {
+        method: 'POST',
+        headers: { authorization: localStorage.getItem('token') },
+      })
+        .then(response => response.json())
+        .then(response => {
+          console.log(response);
+          this.getData();
+        });
+    } else {
+      fetch(`${WISH_URL}${this.state.data.id}/wish`, {
+        method: 'DELETE',
+        headers: { authorization: localStorage.getItem('token') },
+      })
+        .then(response => response.json())
+        .then(response => this.getData());
+    }
+  };
 
   handleDelete = response => {
-    let updateReviewdata = [];
+    let Reviewdata = [];
     for (let i = 0; i <= 4; i++) {
-      if (response.results[0].review[i]) {
-        updateReviewdata = updateReviewdata.concat(
-          response.results[0].review[i]
-        );
+      if (response.results.reviews[i]) {
+        Reviewdata = Reviewdata.concat(response.result.reviews[i]);
       }
     }
-
     this.setState({
-      data: response.results[0],
-      updateReviewdata: [...updateReviewdata],
+      data: response.result,
+      updateReviewdata: [...Reviewdata],
     });
   };
 
   moreButton = () => {
     const { data, quantity } = this.state;
-    let updateReviewdata = [];
-    let reveiw = [...data.review];
-    if (reveiw.length > quantity) {
+    let reviewlength = [];
+    reviewlength = reviewlength.concat(data.reviews);
+    let Reviewdata = [];
+    if (reviewlength.length > quantity) {
       for (let i = 0; i <= quantity; i++) {
-        updateReviewdata = updateReviewdata.concat(data.review[i]);
+        Reviewdata = Reviewdata.concat(data.reviews[i]);
       }
       this.setState({
-        updateReviewdata: [...updateReviewdata],
+        updateReviewdata: [...Reviewdata],
         quantity: quantity + 5,
       });
-    } else if (4 > quantity - reveiw.length > 0) {
-      for (let i = quantity - 4; i < reveiw.length; i++) {
-        updateReviewdata = updateReviewdata.concat(data.review[i]);
+    } else if (4 > quantity - reviewlength.length > 0) {
+      for (let i = quantity - 4; i < reviewlength.length; i++) {
+        Reviewdata = Reviewdata.concat(data.reviews[i]);
       }
       let remain = [...this.state.updateReviewdata];
-      remain = remain.concat(updateReviewdata);
+      remain = remain.concat(Reviewdata);
       this.setState({
         updateReviewdata: [...remain],
         quantity: quantity + 4,
-        moreButtonHidden: true,
+        isMoreButtonHidden: true,
       });
     }
   };
   // mock data
   // componentDidMount() {
-  //   fetch('http://localhost:3000/data/restaurantdetail.json')
+  //   fetch('/data/restaurantdetail.json')
   //     .then(res => res.json())
   //     .then(response => {
-  //       // console.log(response);
   //       let updateReviewdata = [];
   //       for (let i = 0; i <= 4; i++) {
   //         if (response.results[0].review[i]) {
@@ -82,7 +100,13 @@ class ShopDetail extends React.Component {
   //             response.results[0].review[i]
   //           );
   //         }
+  //         if (response.results[0].review.length <= 5) {
+  //           this.setState({
+  //            isMoreButtonHidden: true,
+  //           });
+  //         }
   //       }
+
   //       this.setState({
   //         data: response.results[0],
   //         updateReviewdata: [...updateReviewdata],
@@ -91,58 +115,70 @@ class ShopDetail extends React.Component {
   // }
   // server 연결
   componentDidMount() {
-    fetch(REVIEW_URL, {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch(RESTAURANT_DETAIL_URL, {
       headers: { authorization: localStorage.getItem('token') },
     })
       .then(res => res.json())
       .then(response => {
         let updateReviewdata = [];
         for (let i = 0; i <= 4; i++) {
-          if (response.results[0].review[i]) {
+          if (response.result.reviews[i]) {
             updateReviewdata = updateReviewdata.concat(
-              response.results[0].review[i]
+              response.result.reviews[i]
             );
           }
-          if (response.results[0].review.length <= 5) {
+          if (response.result.reviews.length <= 5) {
             this.setState({
-              moreButtonHidden: true,
+              isMoreButtonHidden: true,
             });
           }
         }
-
         this.setState({
-          data: response.results[0],
+          data: response.result,
           updateReviewdata: [...updateReviewdata],
+          wanted: response.result.is_wished,
         });
       });
-  }
+  };
 
   render() {
-    const { data, updateReviewdata, moreButtonHidden } = this.state;
-    const { name, is_wished, rating } = data;
-    console.log(updateReviewdata);
-    const reviews =
-      updateReviewdata &&
-      updateReviewdata.map(({ description, created_at, images, rating }) => {
+    console.log(this.state.data);
+    const { data, updateReviewdata, isMoreButtonHidden } = this.state;
+    const { name, rating, is_wished } = data;
+    const reviews = updateReviewdata.map(
+      ({ description, created_at, images_url, rating, id, user }) => {
         return (
           <RestaurantReview
+            key={id}
+            review_id={id}
+            user={user}
+            Restaurantid={data.id}
             description={description}
             created_at={created_at}
-            images={images}
+            images={images_url}
             rating={rating}
             handleDelete={this.handleDelete}
+            getData={this.getData}
           />
         );
-      });
-
+      }
+    );
+    const reveiwimages = updateReviewdata.map(({ images_url }) => {
+      return <img alt="스시작이미지" src={images_url} />;
+    });
     return (
       <div className="shopDetail">
         <div>
           <aside className="foodimg">
-            <img alt="스시작이미지" src="/images/shopDetail/1.jpeg" />
+            {/* <img alt="스시작이미지" src="/images/shopDetail/1.jpeg" />
             <img alt="스시작이미지" src="/images/shopDetail/2.jpeg" />
             <img alt="스시작이미지" src="/images/shopDetail/3.jpeg" />
-            <img alt="스시작이미지" src="/images/shopDetail/4.jpeg" />
+            <img alt="스시작이미지" src="/images/shopDetail/4.jpeg" /> */}
+            {reveiwimages}
           </aside>
         </div>
         <div className="inner">
@@ -153,17 +189,19 @@ class ShopDetail extends React.Component {
             </div>
             <div className="restaurantTitleButton">
               <button
-                className="reveiwAndLikeIcon write"
+                className="reviewAndLikeIcon write"
                 onClick={this.goToWrtingPage}
               >
                 <i className="far fa-edit "></i>
                 리뷰쓰기
               </button>
               <button
-                className={`reviewAndLikeIcon ${is_wished ? 'wanted' : ''}`}
+                className={`reviewAndLikeIcon write ${
+                  is_wished ? 'isWanted' : ''
+                }`}
                 onClick={this.handleWanted}
               >
-                <i className={is_wished ? 'fas fa-star ' : 'far fa-star'}></i>
+                <i className={is_wished ? 'fas fa-star' : 'far fa-star'}></i>
                 가고싶다
               </button>
             </div>
@@ -171,10 +209,10 @@ class ShopDetail extends React.Component {
           <RestaurantInfo data={data} />
           <h2 className="restaurantReviewList">
             <span> 리뷰</span>
-            <span className="reviewAllCount">(13)</span>
+            <span className="reviewAllCount">({updateReviewdata.length})</span>
           </h2>
           {reviews}
-          <div className={moreButtonHidden ? 'more hidden' : 'more'}>
+          <div className={isMoreButtonHidden ? 'more hidden' : 'more'}>
             <button className="moreButton" onClick={this.moreButton}>
               5개 더보기 ▼
             </button>
